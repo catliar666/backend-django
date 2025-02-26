85922d9aac0e723d912c5460eb28b3363ca8c725
Maria Ordóñez <maariiaa1912@gmail.com>
2025-02-26 16:11:34 +0100

    corrección error 1

--- backendMattel/settings.py
+++ backendMattel/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() in ["true", "1"]
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["https://backend-django-kkun.onrender.com"]
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ORIGINS", "").split(",")
 
 # Application definition
