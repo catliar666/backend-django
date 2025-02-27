8333ca5fdd6491dc62cbc5a34351da050fbeaf27
Maria Ordóñez <maariiaa1912@gmail.com>
2025-02-27 03:35:22 +0100

    documentacion bonita

--- backendMattel/settings.py
+++ backendMattel/settings.py
@@ -134,13 +134,18 @@ TIME_ZONE = 'Europe/Madrid'
 USE_I18N = True
 USE_TZ = True
 
-# Archivos estáticos
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Archivos multimedia (uploads de imágenes, etc.)
 MEDIA_URL = '/media/'
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
